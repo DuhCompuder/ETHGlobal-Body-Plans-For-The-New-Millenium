c724571ecffb231dfc400b94e040e8f6a9ec0a38
Cristina <cristina@bmu.co>
2022-01-16 11:13:34 -0500

    cristina's files

---
+++ The_Encrabment/multinode_texts.js
@@ -0,0 +1,80 @@
+var multinode_texts= {
+    "daughter": {
+            "part_1_1": {
+                "tag": "anger_fear",
+                "text": "Upon waking on the morning of January 1st, after welcoming the millennium with unrivaled zest, the Borgias found themselves transformed into gigantic crabs. The young daughter, unable to fully determine if she was awake or dreaming, softly passed her left pincer over her hardened torso. As she felt the cartilaginous quality of her being, she moved her insides in a way that, the night before, would have engendered a scream. Now, however, the only thing that came out of her mouth was a moist, almost tender gurgling."
+            },
+            "part_1_2": {
+                "tag": "anger_fear",
+                "text": "In her room, incapable of articulating words, the young daughter raged. Her wrath, enhanced by her arthropod physiology, descended upon her furnitures with biblical indifference. The disembodied neck of Mr. Sourdough, her stuffed giraffe, flew across the air."
+            },
+            "part_2_1":{
+                "tag": "sadness",
+                "text": "Life at the Borgias' house quickly became untenable. Their rooms and corridors and doorways, made to deal with human-shaped dwellers, were laid bare by the deep scarring of arthropod transit. Their things, now a thick layer of wreckage upon the floor, instilled an uncomfortable urgency. By then, the daughter had become obsessed with the putrid smell that permeated the house: the humid trash, the rotting carcasses, the mold. She would hold her pincers against the front of her carapace, where she assumed her nose would be, just to realize that she could still feel the stench being absorbed into her body, through her shell, through her limbs. Her entire self had become privy to the minuscule fragments that inhabited the air. She could feel the smell. Taste it. She had tried to run away from it but soon learned that the world was full of fragrance, and that there was no escaping the omnipresent flavor of things. She stretched her limbs and hugged her bed’s metallic frame. It felt bitter, somewhat salty, and not absent zest. "
+            },
+            "part_2_2":{
+                "tag": "sadness",
+                "text": "Still embracing her bent bed frame, the daughter was overcome with an intense sadness, a sense of hopelessness and loss. It was not that she could not escape the putrid odors of the world, nor that she was forced to feel their taste deep inside her being. It was that she knew that taste was mold, and trash, and carcass, and yet she couldn’t help but enjoy it.  "
+            },
+            "part_3_1":{
+                "tag": "joy",
+                "text": "A weariness had settled deep into the Borgias' souls, and their shells, and their soft, muscular innards. The daughter looked at her half-collapsed room and felt trapped, incarcerated by the remains of her bipedal past. Not knowing what to do, she paused for a minute to inhale, to take in the world. Out of all the flavors in the air, mild and strong, one caught her attention: a faint trail of salt, a mineral moisture. To her, it felt like an invitation. And so she climbed out her window and surrendered to the beckoning of the shores, treading the streets with arthropod elegance."
+            },
+            "part_3_2":{
+                "tag": "joy",
+                "text": "On her way to the sea, the daughter meandered, letting herself be governed by the curiosity of her limbs. She climbed on top of things, hid beneath others. She tore open the door of an abandoned car just to lay inside for the briefest of moments and clung to the side of a stranger’s house just because she could. There was a playfulness coded deep into her constant gurgling; her movement compelled by an engine of joy. As she neared the shore, a light rain fell upon her. The air was pregnant with salt."
+            }
+        },
+    "parents": {
+            "part_1_1": {
+                "tag": "joy",
+                "text": "Upon waking on the morning of January 1st, after welcoming the millennium with unrivaled zest, the Borgias found themselves transformed into gigantic crabs. Still lying in bed and after taking stock of the situation, the mother tenderly moved her pincers to meet her husband’s carcinized body. His deep black eyes, barely protruding from his shell, conveyed an impossibly urgent terror. His limbs, all ten of them, jerked as if unwillingly.  “It’ll be alright, love. We’ll be alright,” she tried to say, but her crustacean throat betrayed her. Instead, a light gurgling came out."
+            },
+            "part_1_2": {
+                "tag": "joy",
+                "text": "Back at the Borgias’ state, the father had found solace in his wife’s temperance. They rose from bed together, claw in claw, and set out to face the world."
+            },
+            "part_2_1":{
+                "tag": "anger_fear",
+                "text": "Life at the Borgias' house quickly became untenable. Their rooms and corridors and doorways, made to deal with human-shaped dwellers, were laid bare by the deep scarring of arthropod transit. Their things, now a thick layer of wreckage upon the floor, instilled an uncomfortable urgency. The mother had been waging war with vermin for days. The mice and cockroaches that she once thought the burden of lesser households had made a home in the now unreachable crevices of her surroundings. Whenever she caught one of the critters scampering away from safe cover, she would crush it under her massive pincer. She felt intoxicated by her own strength, knowing full well that she could just as easily crush a raccoon, or a fox, or a small child. Not that she would ever, she reassured herself. Still, she reveled in the possibility."
+            },
+            "part_2_2":{
+                "tag": "anger_fear",
+                "text": "Back in his room, the father, starved for normalcy, rummaged the remains of his wardrobe for clothes to cover his nakedness. He tried putting on shirts and pants on his multiple limbs, but he lacked the shape and range of motion that those garments so intransigently demanded. After tearing apart the last of his dress shirts, he settled for a baseball cap, which he balanced precariously on top of his shell, between the stems of his eyes. He slowly walked to the window and rejoiced in seeing the civilized reflection of a clothed being. A moment passed, and the cap promptly fell to the floor. He gurgled loudly and, irate, flung himself through the glass, seeking permanent relief in the sidewalk two floors below. The concrete cracked, but his carapace remained unscathed."
+            },
+            "part_3_1":{
+                "tag": "sadness",
+                "text": "A weariness had settled deep into the Borgias' souls, and their shells, and their soft, muscular innards. The father woke up in the middle of the day gasping for something. There was plenty of air but it felt rough, as if breathing minuscule blades, pulverized glass. His body was aching for moisture, and he felt not unlike drowning. The mother, much aware of her watery needs, had barricaded herself in the bathroom and broken every pipe within reach of her pincers. For a while, vital wetness rained upon her, but soon the leaking grew milder, until there was no leaking at all. The father, desperate, had rummaged the remains of the fridge and crushed every container that housed any form of moisture. Jugs of spoiled milk, cans of warm beer, a jar of alfredo sauce. He created a wetness on the floor and wallowed in it. It helped, but he knew full well that it wouldn’t last for long. There was a pull in his body, a hunger for water that hummed inside every crevice of his being."
+            },
+            "part_3_2":{
+                "tag": "sadness",
+                "text": "When the mother emerged from the bathroom, she found her husband gasping on the floor covered in rotten milk and pungent pastes. They gurgled at one another with a tired sadness, recognizing a shared, primal need. And so they both surrendered to the beckoning of the shores, making their pilgrimage together. Behind them, the house they had inhabited for decades disappeared into the horizon, inhospitable, hostile. As they made their way to the ocean, a light rain soothed their barren gills. "
+            }
+        },
+    "son": {
+            "part_1_1": {
+                "tag": "sadness",
+                "text": "Upon waking on the morning of January 1st, after welcoming the millennium with unrivaled zest, the Borgias found themselves transformed into gigantic crabs. The eldest son, still in disbelief, fixated on the improbable shapes of his crustacean body. He thought, quite hopefully, that it was a trick of the light, a half-dream, a stroke of madness. He thought, quite hopefully, that if he took a deep breath and closed his eyes, his fleshy bits would come back running, back to their rightful place upon him. He took a deep breath but, try as he might, his eyes wouldn’t close. He brought his pincers to his hardened face in the hopes of triggering a blink, but his blinking bits were missing and his eyes were rough like nails, like cartilage. He attempted a sob. A gurgling came out."
+            },
+            "part_1_2": {
+                "tag": "sadness",
+                "text": "In his room, the son couldn’t stop looking at his carcinized self. He realized that this was no dream and wondered if he would ever dream again, eyes wide open and all."
+            },
+            "part_2_1":{
+                "tag": "joy",
+                "text": "Life at the Borgias' house quickly became untenable. Their rooms and corridors and doorways, made to deal with human-shaped dwellers, were laid bare by the deep scarring of arthropod transit. Their things, now a thick layer of wreckage upon the floor, instilled an uncomfortable urgency. In the dead of night, while the rest of the family gurgled loudly at each other for reasons he could not discern, the son quietly climbed down the window determined to find comfort in the arms of his beloved. The journey, which before would have taken no longer than fifteen minutes, went on for hours. The streets felt unfamiliar to him, and out in the open he struggled to steer his body with any semblance of elegance. His eyes, high up in their stems, simultaneously offered him all possible directions, and his legs, confused, stumbled upon each other, entangling themselves in painful arrangements. After his seventeenth fall, however, something clicked, and instead of trying to submit each of his limbs to conscious orchestration, he embraced the natural bent of his joints, his panoptic sense of space, and the eager strength of his inner musculature. By dawn, he had become a creature of motion, effortlessly weaving through the undarkened streets. "
+            },
+            "part_2_2":{
+                "tag": "joy",
+                "text": "Once arrived at the house of his beloved, the son found a gigantic crab moistening himself in the middle of two automatic sprinklers. He approached cautiously, unsure if the crab before him was the crab he so desperately wanted to encounter. The moistened crab raised the stems of his eyes and gurgled softly, as if asking a question. The son gurgled back, as if to answer, and they both imagined with hopeful certainty that the other was the one they were expecting. The son lay under the sprinklers, next to his beloved, and felt at home."
+            },
+            "part_3_1":{
+                "tag": "anger_fear",
+                "text": "A weariness had settled deep into the Borgias' souls, and their shells, and their soft, muscular innards. Far from his childhood house, the son had spent several nights frolicking under automatic sprinklers, feeling something he could only think of as love and yet, not the moisture nor the pleasant company relieved him of the aching that had come to besiege his body. He had grown weaker, felt heavier. It was as if his carcinized self craved for a buoyancy that the air denied him. He felt a pull, a hunger for water that the waning sprinklers could not sate. One day, he woke up in the middle of the morning and gurgled at his beloved to try and convey this primal need, but there was no response. He tried to wake him up by gurgling louder, by poking the side of his shell, by hitting him between the eyes. He tried thrusting himself underneath him, tipping him over, but his beloved remained silent. Stiff. The sprinklers leaked, moistening the grass."
+            },
+            "part_3_2":{
+                "tag": "anger_fear",
+                "text": "The son had managed to roll his beloved out of the damp lawn and into the street. It took him hours. He had not forgotten his own seafaring urges but thought, with certainty, that if he managed to submerge his partner in more generous waters, he would wake up, as one does, moist and lively. Each time he would thrust himself underneath him, however, his beloved grew more unwieldy, weightier, until he would not budge at all. When the son found himself incapable of moving him any further, he was overcome with rage. He tried hitting his own carapace with whatever strength he had left but noticed that the bent of his joints only allowed for a light tapping. And so he tapped away, tapping and tapping until his joints went sore. A light rain started to fall. When he finally surrendered to the beckoning of the shores, he made his pilgrimage alone."
+            }
+        }
+}
